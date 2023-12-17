e267f0fb151485d5075fa709f4574873196768ef
jcm098 <30985133+jcm098@users.noreply.github.com>
2023-12-17 17:42:30 +0000

    Working on part 2 of day 1

--- script.js
+++ script.js
@@ -4,29 +4,37 @@ setGlobalVariables();
  function setGlobalVariables() {
    consoleOut("Global Variables set ...");
    var day = document.getElementById("day").value;
+   var dayPart = document.getElementById("dayPart").value;
    var exampleData = document.getElementById("exampleData").value;
    var inputData = document.getElementById("inputData").value;
  }
 
 //load data for the specified day
 function loadData() {
+  let fDay = day.value;
+  let fDayPart = dayPart.value;
+  
   consoleOut("loading data...");
-  $("#exampleData").load("input/day1-example.txt");
-  $("#inputData").load("input/day1-input.txt");
+  let exampleDataFile = "input/day" + fDay + "." + fDayPart + "-example.txt";
+  let inputDataFile = "input/day" + fDay + "-input.txt";
+  consoleOut("exampleDataFile: " + exampleDataFile);
+  consoleOut("inputDataFile: " + inputDataFile);
+  $("#exampleData").load(exampleDataFile);
+  $("#inputData").load(inputDataFile);
 }
 
 //execute example
 function runExample() {
   consoleOut("Running example ...");
   setGlobalVariables();
-  runCode("example", Number(day.value), exampleData.value, inputData.value);
+  runCode("example", Number(day.value), Number(dayPart.value), exampleData.value, inputData.value);
 }
 
 //execute input
 function runInput() {
   consoleOut("Running input ...");
   setGlobalVariables();
-  runCode("input", Number(day.value), exampleData.value, inputData.value);
+  runCode("input", Number(day.value), Number(dayPart.value), exampleData.value, inputData.value);
 }
 
 function consoleOut(output) {
@@ -41,7 +49,7 @@ function consoleOut(output) {
 //code here needs to pass variables the RIGHT WAY
 
 //execute script for the selected day
-function runCode(mode, day, exampleData, inputData) {
+function runCode(mode, day, dayPart, exampleData, inputData) {
   consoleOut("Running code for day " + day + " ...");
   result = "";
   
@@ -55,7 +63,11 @@ function runCode(mode, day, exampleData, inputData) {
 
   switch (day) {
     case 1:
-      result = day1(data);
+      if (dayPart == "1") {
+        result = day1_1(data);
+      } else if (dayPart == "2") {
+        result = day1_2(data);
+      }
       break;
     default:
       break;
@@ -75,9 +87,57 @@ function runCode(mode, day, exampleData, inputData) {
 
 //scripts for dayzzzzz
 
-//day 1
-function day1(data) {
-  consoleOut("Running the day 1 function ...");
-  return "day 1 result";
+//day 1 part 1
+function day1_1(data) {
+  consoleOut("Running the day 1 part 1 function ...");
+
+  let calibValueTotal = 0;
+  
+  //traverse the input data line by line
+  calibDoc = data.split('\n');
+
+  //split the lines out and loop through them
+  for (const calibLine of calibDoc) {
+    consoleOut(calibLine); // Prints each fruit on a new line
+
+    let firstDigit = "";
+    let lastDigit = "";
+    //loop through the characters in each line
+    for (const value of calibLine) {
+      if (!isNaN(value)) {
+
+        if (firstDigit == "") {
+          firstDigit = value;
+        }
+        
+        lastDigit = value;
+      }  
+          
+      //consoleOut(value);
+    }
+
+    consoleOut(firstDigit + lastDigit);
+    calibValueTotal += Number(firstDigit + lastDigit);
+    consoleOut(calibValueTotal);
+  }
+  
+  return calibValueTotal;
 }
 
+//day 1 part 2
+function day1_2(data) {
+  consoleOut(arguments.callee.name + " Running the day 1 part 2 function ...");
+
+  //traverse the input data line by line
+  calibDoc = data.split('\n');
+  
+  //split the lines out and loop through them
+  for (const calibLine of calibDoc) {
+    //consoleOut(calibLine); // Prints each fruit on a new line
+
+    
+    
+  }
+  
+  return day1_1(data); 
+}
